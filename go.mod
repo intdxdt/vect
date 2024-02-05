ea93aac1891a98b4f887caad0216c3e265904903
Titus T <dxtitus@gmail.com>
2024-02-05 08:37:34 +0000

    added go mod 1.21

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/intdxdt/vect
+
+go 1.21
+
+require (
+	github.com/franela/goblin v0.0.0-20211003143422-0a4f594942bf
+	github.com/intdxdt/geom v1.5.2
+	github.com/intdxdt/math v1.2.0
+	github.com/intdxdt/side v1.0.0
+)
+
+require (
+	github.com/intdxdt/algor v1.0.0 // indirect
+	github.com/intdxdt/cmp v1.0.0 // indirect
+	github.com/intdxdt/heap v0.0.0-20180830201411-bfa61463ae8b // indirect
+	github.com/intdxdt/mbr v1.3.0 // indirect
+	github.com/intdxdt/robust v1.0.1 // indirect
+	github.com/intdxdt/sset v1.1.0 // indirect
+	github.com/intdxdt/subsset v1.1.0 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+)
--- vector.go
+++ vector.go
@@ -7,75 +7,75 @@ import (
 
 var feq = math.FloatEqual
 
-//Add creates A new point by adding to other point
+// Add creates A new point by adding to other point
 func (v Vector) Add(o Vector) Vector {
 	return Vector{v[x] + o[x], v[y] + o[y]}
 }
 
-//Add creates A new point by adding to other point
+// Sub creates A new point by adding to other point
 func (v Vector) Sub(o Vector) Vector {
 	return Vector{v[x] - o[x], v[y] - o[y]}
 }
 
-//Is A zero vector
+// IsZero - Is A zero vector
 func (v Vector) IsZero() bool {
 	return feq(v[x], 0) && feq(v[y], 0)
 }
 
-//KProduct create new point by multiplying point by A scaler  k
+// KProduct create new point by multiplying point by A scaler  k
 func (v Vector) KProduct(k float64) Vector {
 	var cx, cy = geom.KProduct(v[x], v[y], k)
 	return Vector{cx, cy}
 }
 
-//Negate vector
+// Neg - Negate vector
 func (v Vector) Neg() Vector {
 	return v.KProduct(-1)
 }
 
-//Computes vector magnitude of pt as vector: x , y as components
+// Magnitude - Computes vector magnitude of pt as vector: x , y as components
 func (v Vector) Magnitude() float64 {
 	return geom.MagnitudeXY(v[x], v[y])
 }
 
-//Computes the square vector magnitude of pt as vector: x , y as components
-//This has A potential overflow problem based on coordinates of pt x^2 + y^2
+// SquareMagnitude - Computes the square vector magnitude of pt as vector: x , y as components
+// This has A potential overflow problem based on coordinates of pt x^2 + y^2
 func (v Vector) SquareMagnitude() float64 {
 	return geom.MagnitudeSquareXY(v[x], v[y])
 }
 
-//Dot Product of two points as vectors
+// Dot Product of two points as vectors
 func (v Vector) DotProduct(o Vector) float64 {
 	return geom.DotProduct(v[x], v[y], o[x], o[y])
 }
 
-//Unit vector of point
+// UnitVector -  Unit vector of point
 func (v Vector) UnitVector() Vector {
 	var cx, cy = geom.UnitVector(v[x], v[y])
 	return Vector{cx, cy}
 }
 
-//Project vector u on V
+// Project - vector u on V
 func (u Vector) Project(v Vector) float64 {
 	return geom.ProjectXY(u[x], u[y], v[x], v[y])
 }
 
-//Dir computes direction in radians - counter clockwise from x-axis.
+// Direction - Dir computes direction in radians - counter clockwise from x-axis.
 func (v Vector) Direction() float64 {
 	return geom.Direction(v[x], v[y])
 }
 
-//Reversed direction of vector direction
+// ReverseDirection - Reversed direction of vector direction
 func (v Vector) ReverseDirection() float64 {
 	return geom.ReverseDirection(v.Direction())
 }
 
-//Computes the deflection angle from vector V to u
+// DeflectionAngle - Computes the deflection angle from vector V to u
 func (v Vector) DeflectionAngle(u Vector) float64 {
 	return geom.DeflectionAngle(v.Direction(), u.Direction())
 }
 
-//Checks if vector has any component as NaN
+// IsNull - Checks if vector has any component as NaN
 func (v Vector) IsNull() bool {
 	return geom.IsNull(v[x], v[y])
 }
